b300417fc97defeb4ce2a5fb44119462e2565e21
Chamano Ramashia <chamano.sr@gmail.com>
2021-04-25 01:18:34 +0200

    javascript celcius to fahreinheit converter

---
+++ script.js
@@ -0,0 +1,26 @@
+const celciusInput = document.querySelector('#celcius > input');
+const fahrenheitInput = document.querySelector('#fahrenheit > input');
+
+
+celciusInput.addEventListener('input',celciusToFahrenheit);
+fahrenheitInput.addEventListener('input',fahrenheitToCelcius);
+
+
+function roundNum(num){
+    return Math.round(num*100)/100;
+}
+
+function celciusToFahrenheit(){
+    const cTemp = parseFloat(celciusInput.value);
+    const fTemp = (cTemp*(9/5) + 32);
+    fahrenheitInput.value = roundNum(fTemp);
+}
+
+function fahrenheitToCelcius(){
+    const fTemp = parseFloat(fahrenheitInput.value);
+    const cTemp = (fTemp - 32)*(5/9);
+    celciusInput.value =roundNum(cTemp);
+}
+
+
+
